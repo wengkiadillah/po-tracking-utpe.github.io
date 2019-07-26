fb339ef8c7540768c24f78c9b127295a82642924
Wengki Adillah <wengki@altrovis.com>
2019-07-26 15:53:10 +0700

    entites edmx

--- POTrackingV2/POTrackingV2/Models/POTracking.Context.cs
+++ POTrackingV2/POTrackingV2/Models/POTracking.Context.cs
@@ -13,10 +13,10 @@ namespace POTrackingV2.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class Entities : DbContext
+    public partial class POTrackingEntities : DbContext
     {
-        public Entities()
-            : base("name=Entities")
+        public POTrackingEntities()
+            : base("name=POTrackingEntities")
         {
         }
     
