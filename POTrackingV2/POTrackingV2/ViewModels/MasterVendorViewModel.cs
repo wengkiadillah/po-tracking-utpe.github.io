26ed64c86661b00c0a02cb9770be0870e652dfbe
arif rahman cahyanda <arifyanda23@gmail.com>
2020-07-08 12:08:22 +0700

    subcontcomponent, delete ongoing

--- POTrackingV2/POTrackingV2/ViewModels/MasterVendorViewModel.cs
+++ POTrackingV2/POTrackingV2/ViewModels/MasterVendorViewModel.cs
@@ -5,15 +5,17 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace POTrackingV2.ViewModels
 {
     public class MasterVendorViewModel
     {
-        [NotMapped]
+        [Required]
         public string SelectedName { get; set; }
 
         public SelectList ListName { get; set; }
+        //public IEnumerable<Vendor> Vendors { get; set; }
 
         public SubcontComponentCapability subCont { get; set; }
 
